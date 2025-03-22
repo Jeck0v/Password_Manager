2ce334c6213075494d4441019751c38f574303ee
Jeck0v <arnaud.fischer75@gmail.com>
2025-03-22 18:39:58 +0100

    :triangular_flag_on_post: add feature

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-mod utils;
-mod config;
-mod db;
 mod ui;
+mod db;
 
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+use ui::{AppController, AppState};
+use eframe::App;
+
+impl App for AppController {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            match self.state {
+                AppState::Calculator => self.calculator.update(ctx, ui, &mut self.state),
+                AppState::Auth => self.auth_screen.update(ui, &mut self.state),
+                AppState::Manager => self.password_manager.update(ui),
+            }
+        });
+    }
+}
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native("Password Manager", options, Box::new(|_cc| Box::new(AppController::default())))
 }
